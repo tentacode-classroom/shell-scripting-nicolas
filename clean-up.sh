3a1d711e5331567fa363efc3bae36d5d2f98f1af
Nicolas Notararigo <nicolas.notararigo@ynov.com>
2018-12-18 17:38:25 +0100

    update clean-up
    script finished

--- clean-up.sh
+++ clean-up.sh
@@ -1,20 +1,36 @@
+
 #!/bin/bash
 
 #Rangement des fichiers
+CURRENT_DIRECTORY=`dirname $0`
+echo current : $CURRENT_DIRECTORY
 
-LIST=`find $1 -print`
+LIST=`find $1 -type f`
+
+rm -rf /clean-up
 
 for ITEM in $LIST
 do
-	if [ "`file $ITEM`" = "`$ITEM`: empty" ]
+	if file "$ITEM" | grep -qE 'image data'
 	then
-		if [ file $ITEM | grep 'image data' ]
-		then
-			echo "prout"
-		else
-			echo "prout"
-		fi
+		mkdir -p clean-up/images
+		cp $ITEM $CURRENT_DIRECTORY/clean-up/images
+	elif file "$ITEM" | grep -qE 'html'
+	then
+		mkdir -p clean-up/html
+		cp $ITEM $CURRENT_DIRECTORY/clean-up/html
+	elif file "$ITEM" | grep -qE 'Media'
+	then
+		mkdir -p clean-up/videos
+		cp $ITEM $CURRENT_DIRECTORY/clean-up/videos
+	elif file "$ITEM" | grep -qE 'ASCII'
+	then
+		mkdir -p clean-up/text
+		cp $ITEM $CURRENT_DIRECTORY/clean-up/text
 	else
-		echo $ITEM": empty"
+		mkdir -p clean-up/divers
+		cp $ITEM $CURRENT_DIRECTORY/clean-up/divers
 	fi
 done
+
+rm -rf $1
